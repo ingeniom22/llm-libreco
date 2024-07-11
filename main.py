3c50dc121eb6e2223c6faac153142063872a42fd
ingeniom22 <jamesmichael0444@gmail.com>
2024-07-11 14:01:13 +0700

    up

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from libreco.algorithms import PinSage
 from libreco.data import DataInfo
+from langchain_core.pydantic_v1 import BaseModel, Field
+from langchain.chat_models.openai import ChatOpenAI
 
 LKPP_MODEL_PATH = "recsys_models/lkpp_model"
 LKPP_MODEL_NAME = "pinsage_model_lkpp"
@@ -14,4 +16,21 @@ lkpp_recsys_model = PinSage.load(
     manual=True,
 )
 
-print(lkpp_recsys_model.search_knn_users(1, 5))
+rec = lkpp_recsys_model.search_knn_users(1, 5)
+print(rec)
+print(type(rec))
+
+class Recommendations(BaseModel):
+    recs: list = Field(description="List of company_id")
+
+model = ChatOpenAI(model="gpt-3.5-turbo-0125", temperature=0)
+structured_llm = model.with_structured_output(Recommendations)
+
+def get_users_interactions():
+    pass    
+
+prompt = ""
+
+
+structured_llm.invoke("Tell me a joke about cats")
+    
